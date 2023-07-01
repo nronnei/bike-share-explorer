a37196ce19e59943f3191d1d7e19ce2ecbaac9b4
Nick Ronnei <nronnei@gmail.com>
2023-07-01 11:38:09 -0400

    cut even moar chars

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,13 @@ import { RecoilRoot } from 'recoil';
 import { InjectionProvider } from './context/injection';
 import { Layout } from './Layout';
 
-const APP_BAR_WIDTH_DESKTOP = 16
 
-export const App = () => {
-  return (
-    <ChakraProvider>
-      <RecoilRoot>
-        <InjectionProvider>
-          <Layout />
-        </InjectionProvider>
-      </RecoilRoot>
-    </ChakraProvider>
-  )
-};
+export const App = () => (
+  <ChakraProvider>
+    <RecoilRoot>
+      <InjectionProvider>
+        <Layout />
+      </InjectionProvider>
+    </RecoilRoot>
+  </ChakraProvider>
+);
